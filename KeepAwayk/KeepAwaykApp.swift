e8d33c917b46adf7c8abcd9a43fa69fb13bab792
Jay Doan <jay@doan.works>
2024-07-07 15:29:43 -0500

    Initial Commit

---
+++ KeepAwayk/KeepAwaykApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepAwaykApp.swift
+//  KeepAwayk
+//
+//  Created by Justin Doan on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct KeepAwaykApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
